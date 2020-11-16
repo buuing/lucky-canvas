cd7afe5c6c24240380b019735f02b008959b7a54
buuing <17778888138@189.cn>
2020-11-16 17:48:52 +0800

    docs: 删除废弃的文档

--- docs/assets/js/23.d8dd28ca.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{431:function(e,t,n){"use strict";n.r(t);var l={components:{LuckyWheel:n(317).default}},s=n(40),u=Object(s.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("LuckyWheel",this._b({},"LuckyWheel",this.$attrs,!1))}),[],!1,null,null,null);t.default=u.exports}}]);
