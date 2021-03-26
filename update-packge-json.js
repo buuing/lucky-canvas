c50762a8edb3b1f8b22fd14b14ddbfbd0b9d6f03
徐涛焘 <xutaotao1108@outlook.com>
2021-03-26 14:55:03 +0800

    chore: add vue2 and vue3 dev options

---
+++ update-packge-json.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const args = process.argv.splice(2);
+const packgeJsonVersion = args[0];
+
+main();
+
+function main () {
+  rimraf('./node_modules', function (err) { // 删除当前目录下的 aaa
+    if (err){
+      console.log(err);
+      return false
+    }
+    updatePackgeJsonFile();
+  });
+};
+
+function updatePackgeJsonFile () {
+  const packgeJsonTemplate = fs.readFileSync(path.join(__dirname, `./package-json-template/package${packgeJsonVersion}.json`), 'utf8');
+  const packgeJsonFilePath = path.join(__dirname, '', '', 'package.json');
+  createFile(packgeJsonFilePath, packgeJsonTemplate);
+}
+
+
+
+function createFile (targetFilePath, content) {
+  fs.writeFile(targetFilePath, content, (err) => {
+    if (err) throw err;
+    console.log('The ' + targetFilePath + ' file has been created!');
+  });
+}
+
