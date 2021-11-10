2db363f5b8d983ff238af0d8adec251226a4986e
ldq <ldq404@qq.com>
2021-11-10 22:00:40 +0800

    build: uniApp pkg upgrade to v0.0.3 fix syntax error of vue-cli created project

--- packages/uni/utils.js
+++ packages/uni/utils.js
@@ -26,12 +26,12 @@ export const changeUnits = (value) => {
 
 export const resolveImage = async (img, canvas, srcName = 'src', resolveName = '$resolve') => {
   let imgObj
-  // #ifdef H5
-  imgObj = new Image()
-  // #endif
-  // #ifndef H5
-  imgObj = canvas.createImage()
-  // #endif
+  // 区分 H5 和小程序
+  if (window) {
+    imgObj = new Image()
+  } else {
+    imgObj = canvas.createImage()
+  }
   // 成功回调
   imgObj.onload = () => {
     img[resolveName](imgObj)
