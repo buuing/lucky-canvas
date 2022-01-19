5aa764a910b92732886fc857e4e576066d9a98ef
404 <ldq404@qq.com>
2022-01-20 00:35:14 +0800

    build: taro 导出老虎机组件

--- packages/taro/react/index.js
+++ packages/taro/react/index.js
@@ -1,2 +1,3 @@
 export { default as LuckyWheel } from './LuckyWheel.js'
 export { default as LuckyGrid } from './LuckyGrid.js'
+export { default as SlotMachine } from './SlotMachine.js'
--- packages/taro/vue/index.js
+++ packages/taro/vue/index.js
@@ -1,2 +1,3 @@
 export { default as LuckyWheel } from './LuckyWheel.vue'
 export { default as LuckyGrid } from './LuckyGrid.vue'
+export { default as SlotMachine } from './SlotMachine.vue'
